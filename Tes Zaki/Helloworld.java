109cace387f334d835fc509ee80bfe2361336052
zaki_yudhistira <zakiyudhistira.itb69@gmail.com>
2023-09-21 13:05:28 +0700

    tambahin folder tes

---
+++ Tes Zaki/Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld
+{
+    public static void main(String[] args)
+    {
+        boolean stop = false;
+        while(!stop) // mulainya program
+        {
+
+        }
+    }
+} 
