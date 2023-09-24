5a6ec5d17c1f9e37d8edc19a357870a6ff2fe2c7
Ibrahim Rasyid <thamrinihsan@gmail.com>
2023-09-24 21:20:36 +0700

    gajadi, salah nangkep maksudnya

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -207,10 +207,5 @@ public class Matrix {
         }
         return determinan;
     }
-    //-----------------------------------HITUNG INVERSE MATRIX----------------------------------//
-    public void inverse () {
-
-    }
-    //---------------------------------JANGAN GANGGU DI ANTARA INI HEHE-------------------------//
 }
 
