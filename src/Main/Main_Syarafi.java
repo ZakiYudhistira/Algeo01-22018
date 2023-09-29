7c10d2ffc40fb5b88472446010c23bf36f72550f
zaki <zakiyudhistira.itb69@gmail.com>
2023-09-29 21:38:15 +0700

    sync

--- src/Main/Main_Syarafi.java
+++ src/Main/Main_Syarafi.java
@@ -9,14 +9,9 @@ public class Main_Syarafi {
         boolean error;
         error = false;
         Matrix m = new Matrix(null, 0, 0);
-<<<<<<< Updated upstream
-        // Matrix m2 = new Matrix(null, 0, 0);
-        Matrix_scan.scan_matriks_keyboard(m, "SPL");
-=======
         Matrix m2 = new Matrix(null, 0, 0);
         Matrix_scan.scan_matriks_keyboard(m, "SPL", error);
         Matrix_scan.scan_matriks_keyboard(m2, "SPL", error);
->>>>>>> Stashed changes
         // m2 = m;
         // m2.display();
         // Interpolasi.Interpolasi_Polinom(m, 9.2);
