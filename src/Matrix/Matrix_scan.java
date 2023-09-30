cb3ec755712a0bc99f4f4eb1f4a83aed2f25b145
zaki <zakiyudhistira.itb69@gmail.com>
2023-10-01 04:40:36 +0700

    created scan from file function

--- src/Matrix/Matrix_scan.java
+++ src/Matrix/Matrix_scan.java
@@ -1,6 +1,12 @@
 package Matrix;
 import java.util.*;
+
+import Functions.regresi;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 // Library berisi fungsi untuk membaca matrix
+import java.io.IOException;
 
 public class Matrix_scan {
     public static Scanner scan = new Scanner(System.in);
@@ -39,7 +45,7 @@ public class Matrix_scan {
             }
             else if(jenis == "DETERMINAN"){
                 int row;
-                System.out.print("Masukkan jumlah baris : ");
+                System.out.print("Masukkan jumlah kolom : ");
                 row = scan.nextInt();
                 while(row <= 0){
                     System.out.print("Masukan tidak valid, tolong masukkan nilai yang valid : ");
@@ -89,7 +95,39 @@ public class Matrix_scan {
             }
         }
     }
-    public static void scan_file(String jenis, String path){
-        
+    public static Matrix scan_file(String file_name){
+        double[][] data_temp = new double[100][100];
+        String current_line;
+        int column=0, row=0,max_column=0;
+        try{
+        File file = new File("test\\"+file_name);
+        Scanner scan_file = new Scanner(file);
+        while(scan_file.hasNextLine()){
+            String[] numbers = (scan_file.nextLine()).split("\\s+");
+            for(column=0;column<numbers.length;column++){
+                data_temp[row][column] = Double.parseDouble(numbers[column]);
+            }
+            max_column = numbers.length;
+            row++;
+        }
+        scan_file.close();
+        double[][] data_matriks = new double[row+1][max_column];
+        int i,j;
+        for(i=0;i<data_matriks.length;i++){
+            for(j=0;j<data_matriks[0].length;j++){
+                data_matriks[i][j] = data_temp[i][j];
+            }
+        }
+        Matrix hasil = new Matrix(data_matriks, row, max_column);
+        return hasil;
+        }catch(FileNotFoundException e){
+            Matrix hasil = new Matrix(null, 0, 0);
+            System.out.println("File tidak ditemukan, pastikan input nama file sudah benar :D");
+            return hasil;
+        }
+    }
+    public static void main(String args[]){
+        Matrix baru = scan_file("tes.txt");
+        baru.display();
     }
 }
