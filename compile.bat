7c5d19f2e40684a26412262a25f353167fc65c13
zaki_yudhistira <zakiyudhistira.itb69@gmail.com>
2023-10-05 03:33:16 +0700

    added test cases SPL and compile

--- compile.bat
+++ compile.bat
@@ -1,9 +1,9 @@
 javac -d bin src/Matrix/Matrix.java
 javac -d bin src/Matrix/Matrix_save.java src/Matrix/Matrix.java
 javac -d bin src/Matrix/Matrix_scan.java src/Matrix/Matrix.java
-javac -d bin src/Functions/Interpolasi_bcb_spline.java src/Matrix/Matrix.java src/Functions/SPL.java src/Functions/Inverse.java
+javac -d bin src/Functions/BcbSpline.java src/Matrix/Matrix.java src/Functions/SPL.java src/Functions/Inverse.java
 javac -d bin src/Functions/Interpolasi.java src/Matrix/Matrix.java src/Functions/SPL.java src/Functions/Inverse.java
 javac -d bin src/Functions/Inverse.java src/Matrix/Matrix.java src/Functions/SPL.java
 javac -d bin src/Functions/regresi.java src/Matrix/Matrix.java src/Functions/SPL.java src/Functions/Inverse.java
 javac -d bin src/Functions/SPL.java src/Matrix/Matrix.java src/Functions/Inverse.java
-javac -d bin src/Main/Main.java src/Matrix/Matrix.java src/Matrix/Matrix_save.java src/Matrix/Matrix_scan.java src/Functions/Interpolasi_bcb_spline.java src/Functions/Interpolasi.java src/Functions/Inverse.java src/Functions/regresi.java src/Functions/SPL.java
+javac -d bin src/Main/Main.java src/Matrix/Matrix.java src/Matrix/Matrix_save.java src/Matrix/Matrix_scan.java src/Functions/BcbSpline.java src/Functions/Interpolasi.java src/Functions/Inverse.java src/Functions/regresi.java src/Functions/SPL.java
